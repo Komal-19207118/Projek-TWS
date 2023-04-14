f84950674801224c3e548522ece469afadd97953
Komal-19207118 <101648964+Komal-19207118@users.noreply.github.com>
2023-04-14 15:07:52 +0700

    Add files via upload

---
+++ oop_dart/lib/class.dart
@@ -0,0 +1,42 @@
+import 'dart:ffi';
+
+abstract class Society {
+  void makan();
+}
+
+
+class Manusia extends Society {
+  String nama = '';
+  Manusia(nm) {
+    this.nama = nm;
+  }
+  void makan(){
+    print('$nama makan nasi');
+  }
+}
+class ManusiaMilenial extends Manusia {
+  String email = '';
+  String _password = '';
+
+  ManusiaMilenial(String email) : super(email);
+  void info() {
+    print('nama: $nama, Email:$email');
+  }
+}
+class Programmer extends ManusiaMilenial {
+  Programmer(String email) : super(email);
+
+  @override
+  void info() {
+    print('Email: $email pemiliknya adalah $nama');
+  }
+
+}
+class Dosen extends ManusiaMilenial {
+  Dosen(String nama) : super(nama);
+
+  @override
+  void info() {
+    print('$nama telah memiliki Email= $email');
+  }
+}
